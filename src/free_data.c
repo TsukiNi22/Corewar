cc94cf6e73507ac4d3b6e79f7212c32dc3c4a7b4
Mathias Dumoulin <mathias.dumoulin@epitech.eu>
2025-04-22 06:11:14 +0200

    feat: update the free data for the new process handeling on the champion

--- src/free_data.c
+++ src/free_data.c
@@ -18,11 +18,23 @@ static int free_ptr(void *ptr)
     return OK;
 }
 
+static int free_champions(void *ptr)
+{
+    champion_t *champion = ptr;
+
+    if (!ptr)
+        return err_prog(PTR_ERR, KO, ERR_INFO);
+    if (delete_array(&champion->process, &free_ptr) == KO)
+        return err_prog(UNDEF_ERR, EPITECH_ERR, ERR_INFO);
+    free(champion);
+    return OK;
+}
+
 int free_data(main_data_t *data)
 {
     if (!data)
         return err_prog(PTR_ERR, EPITECH_ERR, ERR_INFO);
-    if (data->champions && delete_array(&data->champions, &free_ptr) == KO)
+    if (data->champions && delete_array(&data->champions, &free_champions) == KO)
         return err_prog(UNDEF_ERR, EPITECH_ERR, ERR_INFO);
     return OK;
 }
