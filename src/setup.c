8ae6413ba78d2e789abf7bb4118b3b32e0101a75
Mathias Dumoulin <mathias.dumoulin@epitech.eu>
2025-04-22 14:17:40 +0200

    feat (setup): add the error handling for too big champion

--- src/setup.c
+++ src/setup.c
@@ -30,22 +30,24 @@ static int setup_index_champion(main_data_t *data)
     return OK;
 }
 
-static int set_prog(main_data_t *data, char const *file,
-    size_t size, int load_address)
+static int set_prog(main_data_t *data, champion_t *champion, size_t size)
 {
     unsigned char buffer[size];
     int fd = 0;
 
-    if (!data || !file)
+    if (!data || !champion)
         return err_prog(PTR_ERR, KO, ERR_INFO);
-    fd = open(file, O_RDONLY);
+    fd = open(champion->file, O_RDONLY);
     if (fd == KO)
         return err_prog(OP_FILE_ERR, KO, ERR_INFO);
     lseek(fd, 4 + PROG_NAME_LENGTH + 4 + 4 + COMMENT_LENGTH + 4, SEEK_SET);
     if (read(fd, buffer, size) != (int) size)
         return err_prog(READ_FILE_ERR, KO, ERR_INFO);
+    if (size > MEM_SIZE / data->champions->len)
+        return err_system(data, KO, champion->name,
+        "Invalid .cor size, too big");
     for (size_t i = 0; i < size; i++)
-        data->memory[load_address + i] = buffer[i];
+        data->memory[champion->load_address + i] = buffer[i];
     close(fd);
     return OK;
 }
@@ -58,8 +60,7 @@ static int setup_memory(main_data_t *data)
         return err_prog(PTR_ERR, KO, ERR_INFO);
     for (size_t i = 0; i < data->champions->len; i++) {
         champion = data->champions->data[i];
-        if (set_prog(data, champion->file, champion->size,
-            champion->load_address) == KO)
+        if (set_prog(data, champion, champion->size) == KO)
             return err_prog(UNDEF_ERR, KO, ERR_INFO);
     }
     return OK;
