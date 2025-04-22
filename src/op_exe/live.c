ecf05d160a8483b32a10ee324a5a554af9015557
Mathias Dumoulin <mathias.dumoulin@epitech.eu>
2025-04-22 15:48:23 +0200

    feat (op): live instruction

--- src/op_exe/live.c
+++ src/op_exe/live.c
@@ -7,10 +7,17 @@
 
 #include "corewar.h"
 #include "error.h"
+#include <stddef.h>
+#include <stdbool.h>
 
 int op_live(main_data_t *data, champion_t *champion, process_t *process)
 {
+    size_t index = 0;
+
     if (!data || !champion || !process)
         return err_prog(PTR_ERR, KO, ERR_INFO);
+    for (index = 0; data->champions->data[i] != champion; index++);
+    data->live_status[index] = true;
+    data->nbr_live++;
     return OK;
 }
