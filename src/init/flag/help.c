b954ed6beb22e1fc3eae3c7d324305ebbea06794
Mathias Dumoulin <mathias.dumoulin@epitech.eu>
2025-04-22 02:09:58 +0200

    fix (coding-style): fix too much char in line for the help (-h / --help)

--- src/init/flag/help.c
+++ src/init/flag/help.c
@@ -11,14 +11,20 @@ int flag_help(void)
 {
     return my_putstr(STDOUT,
     "USAGE\n"
-    "./corewar [-dump nbr_cycle] [[-n prog_number] [-a load_address] prog_name] ...\n"
+    "./corewar [-dump nbr_cycle] "
+    "[[-n prog_number] [-a load_address] prog_name] ...\n"
     "DESCRIPTION\n"
-    "-dump nbr_cycle dumps the memory after the nbr_cycle execution (if the round isn't\n"
-    "already over) with the following format: 32 bytes/line in hexadecimal (A0BCDEFE1DD3...)\n"
-    "-n prog_number sets the next program's number. By default, the first free number in the\n"
+    "-dump nbr_cycle dumps the memory after the nbr_cycle execution "
+    "(if the round isn't\n"
+    "already over) with the following format: 32 bytes/line in hexadecimal "
+    "(A0BCDEFE1DD3...)\n"
+    "-n prog_number sets the next program's number. "
+    "By default, the first free number in the\n"
     "parameter order\n"
-    "-a load_address sets the next program's loading address. When no address is specified,\n"
-    "optimize the addresses so that the processes are as far away from each other as\n"
+    "-a load_address sets the next program's loading address. "
+    "When no address is specified,\n"
+    "optimize the addresses so that the processes are "
+    "as far away from each other as\n"
     "possible. The addresses are MEM_SIZE modulo.\n"
     );
 }
